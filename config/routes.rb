dd2a333d06a76f3b42d855629eb4902f43d01b32
k-buffum <knbuffum@gmail.com>
2016-04-15 19:02:24 -0700

    About/Projects/Contact pages added

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #get 'home' => 'home#index'
+  get 'about' => 'about#about'
+  get 'education' => 'education#education'
+  get 'contact' => 'contact#contact'
+  get 'projects' => 'projects#projects'
+  #get 'books' => 'books#books'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
